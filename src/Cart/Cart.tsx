c8d3b586e95635c67e01e0567e0575940afd258b
DungHoang <kathymaidung@gmail.com>
2021-04-14 14:32:24 +0300

    Add total price

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -10,6 +10,9 @@ type Props = {
 }
 
 export default function Cart({ cartItems, addToCart, removeFromCart }: Props) {
+    const calculateTotal = (items: CartItemType[]) =>
+        items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+
     return (
         <Wrapper>
             <h2>Your Shopping Cart</h2>
@@ -22,6 +25,7 @@ export default function Cart({ cartItems, addToCart, removeFromCart }: Props) {
                     removeFromCart={removeFromCart}
                 />
             ))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }
