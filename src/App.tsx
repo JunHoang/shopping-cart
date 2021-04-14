8a13a00c4b564aa71b6435224bf07e54fbedad1c
DungHoang <kathymaidung@gmail.com>
2021-04-14 13:42:39 +0300

    Add CartItem and addToCart

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 //Styles
 import { Wrapper, StyledButton } from './App.styles';
 
@@ -29,9 +30,25 @@ function App() {
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   console.log(data);
 
-  const getTotalItems = (items: CartItemType[]) => null;
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      //1. Is the item already added in the cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+
+      if (isItemInCart) {
+        return prev.map(item =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 } : item
+        );
+      }
+
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }]
+    })
+  };
 
   const handleRemoveFromCart = () => null;
 
@@ -42,7 +59,7 @@ function App() {
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color='error'>
